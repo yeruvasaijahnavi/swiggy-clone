3124d3dc5e988e2a4c1dc7abc3834e12151361d5
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-11-04 22:26:58 +0530

    Add cart service code

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class CartService {
+	private cartItems: any[] = [];
+	constructor() {}
 
-  constructor() { }
+	addToCart(item: any) {
+		this.cartItems.push(item);
+	}
+
+	removeFromCart(item: any) {
+		this.cartItems = this.cartItems.filter(
+			(cartItem) => cartItem.id !== item.id
+		);
+	}
+
+	getCartItems() {
+		return this.cartItems;
+	}
+
+	calculateTotalPrice() {
+		return this.cartItems.reduce((total, item) => total + item.price, 0);
+	}
 }
