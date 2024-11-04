fc3aa15e09983423065a95892075e8950a947202
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-11-04 19:08:37 +0530

    Add basic favorites ver 2

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-favorites',
-  standalone: true,
-  imports: [],
-  templateUrl: './favorites.component.html',
-  styleUrl: './favorites.component.css'
+	selector: "app-favorites",
+	standalone: true,
+	imports: [],
+	templateUrl: "./favorites.component.html",
+	styleUrl: "./favorites.component.css",
 })
 export class FavoritesComponent {
+	favorites: any[] = []; // Array to hold favorite foods
 
+	constructor() {
+		this.loadFavorites();
+	}
+
+	// Method to load favorites from localStorage
+	loadFavorites() {
+		const storedFavorites = localStorage.getItem("favorites");
+		this.favorites = storedFavorites ? JSON.parse(storedFavorites) : [];
+	}
 }
