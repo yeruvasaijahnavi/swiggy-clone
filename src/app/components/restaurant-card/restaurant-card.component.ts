a1f95cb919eca54a61e865850c6f1fc34a57bfa2
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-11-05 10:56:28 +0530

    Generate rest-card comp

---
+++ src/app/components/restaurant-card/restaurant-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurant-card.component.html',
+  styleUrl: './restaurant-card.component.css'
+})
+export class RestaurantCardComponent {
+
+}
